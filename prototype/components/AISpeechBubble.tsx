ded6931746a0b6910cdd8097cdf79744f296e711
ronnieshi79 <ronnie.shi79@gmail.com>
2026-03-14 14:36:10 +0800

    init project:airobot-tablet

--- prototype/components/AISpeechBubble.tsx
+++
@@ -1,4 +0,0 @@
-
-// This component is now deprecated in favor of VoiceDialoguePanel during active interaction
-// and can be used for general notifications if needed, but we keep it minimal for now.
-export default () => null;
